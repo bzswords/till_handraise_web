046d748647fd1c02a4db802cb4d64b07d714a4fd
Bryan Swords <bzswords@gmail.com>
2018-11-04 12:07:36 -0500

    formatted dates and post card styles

--- app/controllers/buttercms/posts_controller.rb
+++ app/controllers/buttercms/posts_controller.rb
@@ -1,6 +1,7 @@
 class Buttercms::PostsController < Buttercms::BaseController
   def index
-    @posts = ButterCMS::Post.all(:page => 1, :page_size => 4)
+    @posts = ButterCMS::Post.all(:page => 1, :page_size => 4).to_a
+    @featured = @posts.shift
   end
 
   def show
