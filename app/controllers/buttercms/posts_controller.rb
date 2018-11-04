ff2441bfd0dce0757ac6a634ab900139ac1b6a18
Bryan Swords <bzswords@gmail.com>
2018-11-04 14:35:38 -0500

    posts

--- app/controllers/buttercms/posts_controller.rb
+++ app/controllers/buttercms/posts_controller.rb
@@ -1,10 +1,21 @@
 class Buttercms::PostsController < Buttercms::BaseController
+  helper_method :unique_posts
+
   def index
     @posts = ButterCMS::Post.all(:page => 1, :page_size => 4).to_a
     @featured = @posts.shift
   end
 
   def show
-    @post = ButterCMS::Post.find(params[:slug])
+    @featured = ButterCMS::Post.find(params[:slug])
+    @posts = ButterCMS::Post.all(:page => 1, :page_size => 4).to_a
+  end
+
+  private
+
+  def unique_posts(featued, posts)
+    posts.select do |post|
+      post.title != featued.title
+    end
   end
 end
